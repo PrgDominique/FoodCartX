e755c349b96a98282f4588b09035bef190c3b926
PrgDominique <dominiquesimbillo10@gmail.com>
2022-10-28 14:04:15 +0800

    fix overwords in Header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Kainan ni Domz</h1>
+        <h1>Food ni Domz</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
